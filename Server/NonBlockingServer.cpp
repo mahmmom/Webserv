6d3ebea232b412110ce1b1962f4afff6165b0c21
ahmad-kanbari <ahmadkanbari.personal@gmail.com>
2024-10-03 08:25:56 +0400

    adding linux and max to server not tested in linux

--- Server/NonBlockingServer.cpp
+++ Server/NonBlockingServer.cpp
@@ -12,200 +12,223 @@
 
 int NonBlockingServer::count = 0;
 
-NonBlockingServer::NonBlockingServer(int port) : eventlist(10)
+NonBlockingServer::NonBlockingServer(int port)
 {
+    #ifdef __APPLE__
+        eventlist.resize(MAX_EVENTS);
+    #elif __linux__
+        events.resize(MAX_EVENTS);
+    #endif
+
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0)
-    {
+    if (server_socket < 0) {
         throw SocketException();
     }
 
     struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
     opt = 1;
-
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
-    {
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         throw SetsockoptException();
     }
 
-    if(bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
-    {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         throw BindException();
     }
 
-    if (listen(server_socket, 5) < 0)
-    {
+    if (listen(server_socket, BACKLOG) < 0) {
         throw ListenException();
     }
 
     setNonBlocking(server_socket);
 
-	if ((kq = kqueue()) == -1) {
-		perror("Kqueue: ");
-		exit(EXIT_FAILURE);
-	}
-    struct kevent change_event;
-	EV_SET(&change_event, server_socket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-	changelist.push_back(change_event);
-	kevent(kq, changelist.data(), changelist.size(), NULL, 0, 0);
+    #ifdef __APPLE__
+        kq = kqueue();
+        if (kq == -1) {
+            perror("kqueue");
+            exit(EXIT_FAILURE);
+        }
+        struct kevent change_event;
+        EV_SET(&change_event, server_socket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+        changelist.push_back(change_event);
+        kevent(kq, &changelist[0], changelist.size(), NULL, 0, NULL);
+    #elif __linux__
+        epoll_fd = epoll_create(MAX_EVENTS);
+        if (epoll_fd == -1) {
+            perror("epoll_create");
+            exit(EXIT_FAILURE);
+        }
+        struct epoll_event event;
+        event.events = EPOLLIN;
+        event.data.fd = server_socket;
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_socket, &event) == -1) {
+            perror("epoll_ctl");
+            exit(EXIT_FAILURE);
+        }
+    #endif
 }
 
-void NonBlockingServer::setNonBlocking(int socket)
-{
+void NonBlockingServer::setNonBlocking(int socket) {
     int flags = fcntl(socket, F_GETFL, 0);
     fcntl(socket, F_SETFL, flags | O_NONBLOCK);
 }
 
-void NonBlockingServer::acceptNewClient()
-{
-    struct sockaddr_in  client_addr;
-    socklen_t           client_len = sizeof(client_addr);
-    char				client_str_address[INET_ADDRSTRLEN]; // consider adding to client class
+void NonBlockingServer::acceptNewClient() {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char client_str_address[INET_ADDRSTRLEN];
 
     memset(&client_addr, 0, sizeof(client_addr));
     int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
     if (client_socket == -1) {
-        throw(AcceptException());
+        throw AcceptException();
     }
     setNonBlocking(client_socket);
     clients.insert(std::make_pair(client_socket, Client(client_socket)));
-    inet_ntop(client_addr.sin_family, &(client_addr.sin_addr),
-        client_str_address, sizeof(client_str_address));
+    inet_ntop(client_addr.sin_family, &(client_addr.sin_addr), client_str_address, sizeof(client_str_address));
 
     std::cout << "New client connected from: " << client_str_address 
-            << " on fd (" << client_socket << ")" << std::endl;
+              << " on fd (" << client_socket << ")" << std::endl;
 
-    struct kevent change_event;
-    EV_SET(&change_event, client_socket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-    changelist.push_back(change_event);
-    kevent(kq, changelist.data(), changelist.size(), NULL, 0, 0);
+    #ifdef __APPLE__
+        struct kevent change_event;
+        EV_SET(&change_event, client_socket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+        changelist.push_back(change_event);
+        kevent(kq, &changelist[0], changelist.size(), NULL, 0, NULL);
+    #elif __linux__
+        struct epoll_event event;
+        event.events = EPOLLIN | EPOLLET;
+        event.data.fd = client_socket;
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &event) == -1) {
+            perror("epoll_ctl: client_socket");
+            exit(EXIT_FAILURE);
+        }
+    #endif
 }
 
-void NonBlockingServer::handleClientRead(int client_socket)
-{
-    char buffer[MAXDATASIZE] = {0};
+void NonBlockingServer::handleClientRead(int client_socket) {
+    char buffer[MAXDATASIZE];
+    memset(buffer, 0, MAXDATASIZE);
     int bytes_read = recv(client_socket, buffer, sizeof(buffer), 0);
 
     if (bytes_read < 0 && errno != EWOULDBLOCK) {
-        perror ("Recv"); // probably should remove this
-        std::cerr << "Will proceed to disconnect client (" 
-            << client_socket << ")" << std::endl;
+        std::cerr << "Will proceed to disconnect client (" << client_socket << ")" << std::endl;
         to_remove.push_back(client_socket);
-    }
-    else if (bytes_read < 0 && errno == EWOULDBLOCK) {
-        perror("*Recv");
     }
     else if (bytes_read == 0) {
         to_remove.push_back(client_socket);
     }
-    else
-    {
+    else if (bytes_read > 0) {
         std::map<int, Client>::iterator it = clients.find(client_socket);
-        if (it != clients.end())
-        {
+        if (it != clients.end()) {
             buffer[bytes_read] = '\0';
             it->second.getBuffer().append(buffer, bytes_read);
-            std::cout << "Recieved from "
-					<< "(" << it->second.getSocket() << ")"
-					<< " the following -> " << it->second.getBuffer()
-					<< " (bytes recieved: " << bytes_read << ")"
-					<< std::endl << std::endl;
+            std::cout << "Received from (" << it->second.getSocket() << ") -> " 
+                      << it->second.getBuffer() << " (bytes received: " << bytes_read << ")" << std::endl;
 
             HTTPRequest Request(buffer);
 
-            struct kevent change_event;
-            EV_SET(&change_event, client_socket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
-            templist.push_back(change_event);
+            #ifdef __APPLE__
+                struct kevent change_event;
+                EV_SET(&change_event, client_socket, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, NULL);
+                changelist.push_back(change_event);
+            #elif __linux__
+                struct epoll_event event;
+                event.events = EPOLLOUT | EPOLLET;
+                event.data.fd = client_socket;
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, client_socket, &event) == -1) {
+                    perror("epoll_ctl: client_socket");
+                    exit(EXIT_FAILURE);
+                }
+            #endif
         }
     }
 }
 
-void NonBlockingServer::handleClientWrite(int client_socket)
-{
+void NonBlockingServer::handleClientWrite(int client_socket) {
     std::map<int, Client>::iterator it = clients.find(client_socket);
-    if (it != clients.end())
-    {
+    if (it != clients.end()) {
         std::string buffer_head = "Relay from server: ";
         std::string& buffer = it->second.getBuffer();
         int bytes_sent = send(client_socket, (buffer_head + buffer).c_str(), (buffer_head + buffer).length(), 0);
         if (bytes_sent == -1 && errno != EWOULDBLOCK) {
-            perror ("Send"); // probably should remove this
-            std::cerr << "Will proceed to disconnect client (" 
-                << client_socket << ")" << std::endl;          
+            std::cerr << "Will proceed to disconnect client (" << client_socket << ")" << std::endl;          
             to_remove.push_back(client_socket);
         } 
-        else if ((bytes_sent == -1 && errno == EWOULDBLOCK)) {
-            perror("*Send");
+        else if (bytes_sent > 0) {
+            std::cout << "Successfully sent" << std::endl;
+            buffer.erase(0, bytes_sent - buffer_head.length());
         }
-        else {
-            std::cout << "successfuly sent" << std::endl;
-            buffer.erase(0, bytes_sent);
-        }
-        struct kevent change_event;
-        EV_SET(&change_event, client_socket, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
-        templist.push_back(change_event);
+
+        #ifdef __APPLE__
+            struct kevent change_event;
+            EV_SET(&change_event, client_socket, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
+            changelist.push_back(change_event);
+        #elif __linux__
+            struct epoll_event event;
+            event.events = EPOLLIN | EPOLLET;
+            event.data.fd = client_socket;
+            if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, client_socket, &event) == -1) {
+                perror("epoll_ctl: client_socket");
+                exit(EXIT_FAILURE);
+            }
+        #endif
     }
 }
 
-void NonBlockingServer::removeDisconnectedClients()
-{
-    for (size_t i = 0; i < to_remove.size(); ++i)
-    {
-        int socket = to_remove[i];
-        close(socket);  // manual says EV_DELETE: Removes the event from the kqueue. 
-                        // Events which are attached to file descriptors are automatically 
-                        // deleted on the last close of the descriptor. So we can just close
-                        // and any events attached to that the socket are automatically dequeued.
-        
-        clients.erase(socket);  // erase doesn't need an iterator here because we are working with
-                                // maps; for std::maps, we can erase an element by its key because 
-                                // no duplicate keys are allowed. But for a vector, we can have 
-                                // duplicate values and thus, the erase method (its version of the
-                                // override) requires an iterator.
-        std::cout << "Client disconnected: " << socket << std::endl << std::endl;
+void NonBlockingServer::removeDisconnectedClients() {
+    for (std::vector<int>::iterator it = to_remove.begin(); it != to_remove.end(); ++it) {
+        close(*it);
+        clients.erase(*it);
+        std::cout << "Client disconnected: " << *it << std::endl;
     }
     to_remove.clear();
 }
 
-void NonBlockingServer::run()
-{
-    while (true)
-    {
-        // std::cout << "New horde" << std::endl;
-        int nev = kevent(kq, NULL, 0, eventlist.data(), eventlist.size(), NULL);
-        for (int i = 0; i < nev; i++) {
-            if (eventlist[i].filter == EVFILT_READ && eventlist[i].ident == (uintptr_t) server_socket) {
-                acceptNewClient();
+void NonBlockingServer::run() {
+    while (true) {
+        #ifdef __APPLE__
+            int nev = kevent(kq, &changelist[0], changelist.size(), &eventlist[0], eventlist.size(), NULL);
+            changelist.clear();
+            for (int i = 0; i < nev; i++) {
+                if (eventlist[i].ident == (uintptr_t)server_socket) {
+                    acceptNewClient();
+                }
+                else if (eventlist[i].filter == EVFILT_READ) {
+                    handleClientRead(eventlist[i].ident);
+                }
+                else if (eventlist[i].filter == EVFILT_WRITE) {
+                    handleClientWrite(eventlist[i].ident);
+                }
             }
-            else if (eventlist[i].filter == EVFILT_READ && eventlist[i].ident != (uintptr_t) server_socket) {
-                handleClientRead(eventlist[i].ident);
+        #elif __linux__
+            int nfds = epoll_wait(epoll_fd, &events[0], MAX_EVENTS, -1);
+            for (int n = 0; n < nfds; ++n) {
+                if (events[n].data.fd == server_socket) {
+                    acceptNewClient();
+                } else if (events[n].events & EPOLLIN) {
+                    handleClientRead(events[n].data.fd);
+                } else if (events[n].events & EPOLLOUT) {
+                    handleClientWrite(events[n].data.fd);
+                }
             }
-            else if (eventlist[i].filter == EVFILT_WRITE) {
-                handleClientWrite(eventlist[i].ident);
-            }
-        }
-        if (!templist.empty()) {
-            kevent(kq, templist.data(), templist.size(), NULL, 0, 0);
-            templist.clear();
-        }
-        if ((size_t) nev == eventlist.size()) {
-            eventlist.resize(eventlist.size() * 2);  // Double the size if full
-        }
+        #endif
         removeDisconnectedClients();
-        // std::cout << std::endl;
     }
 }
 
-NonBlockingServer::~NonBlockingServer()
-{
+NonBlockingServer::~NonBlockingServer() {
     close(server_socket);
-    std::map<int, Client>::iterator it;
-    for (it = clients.begin(); it != clients.end(); ++it)
-    {
+    for (std::map<int, Client>::iterator it = clients.begin(); it != clients.end(); ++it) {
         close(it->first);
     }
+    #ifdef __APPLE__
+        close(kq);
+    #elif __linux__
+        close(epoll_fd);
+    #endif
 }
--- Server/NonBlockingServer.hpp
+++ Server/NonBlockingServer.hpp
@@ -3,50 +3,61 @@
 
 #include <map>
 #include <vector>
-#include <sys/select.h>
 #include "Client.hpp"
 
 #include <sys/socket.h>
-#include <netinet/in.h>	// for using struct sockaddr_in
-#include <arpa/inet.h> // for inet_pton
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <cstdlib>
 #include <cstring>
-#include <cstdio> // for perror
-#include <unistd.h> // for close
+#include <cstdio>
+#include <unistd.h>
 #include <iostream>
 #include <iterator>
-#include <sys/event.h>  // for kqueue
-#include <sys/types.h>
-#include <algorithm> // for find
+#include <algorithm>
 #include <fcntl.h>
+
+#ifdef __APPLE__
+    #include <sys/event.h>
+#elif __linux__
+    #include <sys/epoll.h>
+#else
+    #error "Unsupported platform"
+#endif
 
 #define PORT 9000
 #define BACKLOG 10
 #define MAXDATASIZE 1024
+#define MAX_EVENTS 10
 
-class NonBlockingServer
-{
-    private:
-        static int count;
-        int server_socket;
-        int opt;
-        int kq; // check if it was implemented as part of the class
+class NonBlockingServer {
+private:
+    static int count;
+    int server_socket;
+    int opt;
+    
+    #ifdef __APPLE__
+        int kq;
         std::vector<struct kevent> changelist;
-	    std::vector<struct kevent> eventlist;
-        std::vector<struct kevent> templist;
-        std::map<int, Client> clients;
-        std::vector<int> to_remove;
+        std::vector<struct kevent> eventlist;
+    #elif __linux__
+        int epoll_fd;
+        std::vector<struct epoll_event> events;
+    #endif
+    
+    std::map<int, Client> clients;
+    std::vector<int> to_remove;
 
-        void setNonBlocking(int socket);
-        void acceptNewClient();
-        void handleClientRead(int client_socket);
-        void handleClientWrite(int client_socket);
-        void removeDisconnectedClients();
+    void setNonBlocking(int socket);
+    void acceptNewClient();
+    void handleClientRead(int client_socket);
+    void handleClientWrite(int client_socket);
+    void removeDisconnectedClients();
 
-    public:
-        NonBlockingServer(int port);
-        void run();
-        ~NonBlockingServer();
+public:
+    NonBlockingServer(int port);
+    void run();
+    ~NonBlockingServer();
 };
 
 #endif
